b5ea32dbe1898697b63ef81145fd396a7abdeef9
Ishaan Gupta <igupta097@gmail.com>
2020-07-23 21:51:12 +0530

    Updates

---
+++ precache-manifest.c8634b73c60cc9b38f77aa393930cc98.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4872a3015fd9791264b07af1fe5818c0",
+    "url": "/robofriends-learn-react/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends-learn-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4c96853ce72aecc90188",
+    "url": "/robofriends-learn-react/static/css/main.821e5383.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robofriends-learn-react/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends-learn-react/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c96853ce72aecc90188",
+    "url": "/robofriends-learn-react/static/js/main.592acf5f.chunk.js"
+  },
+  {
+    "revision": "71f366b34306e73c3b1a",
+    "url": "/robofriends-learn-react/static/js/runtime-main.9ac9c86d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-learn-react/static/media/SEGA.12a3db8b.woff"
+  }
+]);
