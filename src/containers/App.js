f7a1e7a19345ff4345dbdfc7a0fcf0f03ac956a7
Ishaan Gupta <igupta097@gmail.com>
2020-07-24 00:45:08 +0530

    redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,35 +5,37 @@ import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
 import ErrorBoundary from '../components/ErrorBoundary'
 
+import {setSearchField, requestRobots} from '../actions'
+import {connect} from 'react-redux'
+
 import './App.css'
 
-
+const mapStateToProps = state =>{
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}
+const mapDispatchToProps= (dispatch) =>{
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: ()=> dispatch(requestRobots())
+    }
+}
 
 class App extends React.Component
 {
-    constructor()
-    {
-        super();
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
-    }
-
-    onSearchChange = (event) =>
-    {
-       this.setState({searchfield: event.target.value})        
-    }
-
-
     render()
     {
-        const {robots, searchfield} = this.state;
+        const {searchField, onSearchChange, robots, isPending} = this.props;
+        
         const filteredRobots =robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
         })
 
-        if (!robots.length)
+        if (isPending)
         {
             return <h1>Loading</h1>
         }
@@ -41,7 +43,7 @@ class App extends React.Component
         return (
             <div className="tc">
                 <h1 className="f1">RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundary>
                         <CardList robots={filteredRobots}/>
@@ -53,10 +55,8 @@ class App extends React.Component
     }
     componentDidMount()
     {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response=>response.json())
-        .then(users => this.setState({robots:users}))
+        this.props.onRequestRobots();
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
